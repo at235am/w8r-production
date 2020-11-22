70c386ff5e001e0073c87901d0f6a78da91cbcf2
at235am <at235am@gmail.com>
2020-11-22 04:33:56 -0800

    sms production ready

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc8b396d71424ad3c083bfc094544796.js"
+  "/precache-manifest.9e50c06efa5ea05499d7a9dbe814a43c.js"
 );
 
 self.addEventListener('message', (event) => {
